afae419cae86b4025f97bf4567322338ad880591
ILakshitha <ilakshitha7921@gmail.com>
2024-08-23 17:07:52 +0530

    Create folder stucture

---
+++ app/libraries/Core.php
@@ -0,0 +1,19 @@
+<?php
+     class Core{
+        //URL format --> /controller/method/params
+        protected $currentController = 'Pages';
+        protected $currentMethod = 'index';
+        protected $param = [];
+
+        public function __construct(){
+            $this->getURL();
+
+        }
+
+        public function getURL(){
+            echo $_GET['url'];
+        }
+
+     }
+
+?>
