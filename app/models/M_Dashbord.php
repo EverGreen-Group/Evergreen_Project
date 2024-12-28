66145981dc4414641cd37c65567f73c606d1c963
ILakshitha <ilakshitha7921@gmail.com>
2024-12-29 01:53:03 +0530

    TFMS-3 change mysql query in model

--- app/models/M_Dashbord.php
+++ app/models/M_Dashbord.php
@@ -11,9 +11,22 @@ class M_stockvalidate
 
     public function getvalidateStocks()
     {
-        $this->db->query("SELECT s.collection_id, s.driver_id, s.status, s.created_at, c.total_quantity
-                        FROM stockvalidate s, collections c
-                         WHERE s.collection_id = c.collection_id;");
+        $this->db->query("SELECT 
+    CONCAT(u.first_name, ' ', u.last_name) AS full_name,
+    s.collection_id,
+    s.status,
+    s.created_at,
+    c.total_quantity
+    FROM 
+    stockvalidate s
+    JOIN 
+    collections c ON s.collection_id = c.collection_id
+    JOIN 
+    collection_schedules cs ON c.schedule_id = cs.schedule_id
+    JOIN 
+    drivers d ON cs.driver_id = d.driver_id
+    JOIN 
+    users u ON d.user_id = u.user_id;");
         return $this->db->resultSet();
     }
 }
